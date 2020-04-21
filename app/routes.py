833fe8b1d311a219193a8c9b92c4c5d3b89c58e4
Julian Grimm <grimmjulian225@gmail.com>
2020-04-21 15:05:22 +0200

    Added Password Reset

--- app/routes.py
+++ app/routes.py
@@ -12,6 +12,9 @@ from app.forms import EditProfileForm
 from app.forms import RegistrationForm
 from app.forms import ProjectForm
 from app.models import Project
+from app.forms import ResetPasswordRequestForm
+from app.email import send_password_reset_email
+from app.forms import ResetPasswordForm
 
 @app.before_request
 def before_request():
@@ -122,3 +125,31 @@ def edit_profile():
         form.username.data = current_user.username
         form.about_me.data = current_user.about_me
     return render_template('edit_profile.html', title='Edit Profile', form=form)
+
+@app.route('/reset_password_request', methods=['GET', 'POST'])
+def reset_password_request():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = ResetPasswordRequestForm()
+    if form.validate_on_submit():
+        user = User.query.filter(func.upper(User.email) == func.upper(form.email.data)).first()
+        if user:
+            send_password_reset_email(user)
+        flash('Check your email for the instructions to reset your password!')
+        return redirect(url_for('login'))
+    return render_template('reset_password_request.html', title = 'Reset Password', form=form)
+
+@app.route('/reset_password/<token>', methods=['GET','POST'])
+def reset_password(token):
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    user = User.verify_reset_password_token(token)
+    if not user:
+        return redirect(url_for('index'))
+    form = ResetPasswordForm()
+    if form.validate_on_submit():
+        user.set_password(form.password.data)
+        db.session.commit()
+        flash('Your password has been reset.')
+        return redirect(url_for('login'))
+    return render_template('reset_password.html', form = form)
